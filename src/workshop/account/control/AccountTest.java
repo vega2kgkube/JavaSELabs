67bd38c268edeeb0343f633bf2f7f4c1adf54116
vega2kgkube <vega2kgkube@gmail.com>
2025-08-06 14:38:40 +0900

    생성자overloading call

--- src/workshop/account/control/AccountTest.java
+++ src/workshop/account/control/AccountTest.java
@@ -5,13 +5,17 @@ import workshop.account.entity.Account;
 public class AccountTest {
 
 	public static void main(String[] args) {
-		//Account 객체생성
+		//1. Account 객체생성 - 기본생성자 호출
 		Account account = new Account();
 		
+		//1-1. setter 메서드 호출로 값을 대입
 		//고객번호 : "A1100", 계좌번호 : "221-22-3477", 잔액 : 100
 		account.setCustId("A1100");
 		account.setAcctId("221-22-3477");
+		account.deposit(1000);
 		
+		//2.Account 객체생성 - 오버로딩된 생성자 호출
+		new Account("B1200", "331-22-3477", 2000);
 		
 		
 	}
--- src/workshop/account/entity/Account.java
+++ src/workshop/account/entity/Account.java
@@ -4,29 +4,28 @@ public class Account {
 	private String custId;
 	private String acctId;
 	private int balance;
-	
-	//default constructor 선언
+
+	// default constructor 선언
 	public Account() {
 		System.out.println("기본생성자 호출됨");
 	}
-	
-	//Constructor Overloading (생성자 중복정의)
+
+	// Constructor Overloading (생성자 중복정의)
 	public Account(String custId, String acctId, int balance) {
-		super();
-		this.custId = custId;
-		this.acctId = acctId;
+		//this.custId = custId;
+		//this.acctId = acctId;
+		setCustId(custId);
+		setAcctId(acctId);
 		this.balance = balance;
 	}
-	
+
 //	public void setBalance(int balance) {
 //		this.balance = balance;
 //	}
-	
+
 	public int getBalance() {
 		return balance;
 	}
-
-
 
 	public String getCustId() {
 		return custId;
@@ -35,20 +34,21 @@ public class Account {
 	public void setCustId(String custId) {
 		this.custId = custId;
 	}
-	
+
 	public void setAcctId(String acctId) {
 		this.acctId = acctId;
 	}
-	
+
 	public String getAcctId() {
 		return acctId;
 	}
-	
-	//입금
+
+	// 입금
 	public void deposit(int amount) {
 		this.balance += amount;
 	}
-	//출금
+
+	// 출금
 	public void withdraw(int amount) {
 		this.balance -= amount;
 	}
