ed2789f8999249b4d912224d8a3df5322f626479
vega2kgkube <vega2kgkube@gmail.com>
2025-08-07 09:08:08 +0900

    Student 추가

--- src/workshop/person/control/PersonManager.java
+++ src/workshop/person/control/PersonManager.java
@@ -3,8 +3,9 @@ package workshop.person.control;
 public class PersonManager {
 
 	public static void main(String[] args) {
+		PersonManager personMgr = new PersonManager();
 		
-
+		personMgr.printTitle("인물정보 조회시스템");
 	}
 	
 	public void printTitle(String title) {
