eea3061595f4b5fc8f64d5f5b0d95a1eb8b47d3a
vega2kgkube <vega2kgkube@gmail.com>
2025-08-07 16:02:50 +0900

    flexible update

--- src/workshop/oop/flexible/Employee.java
+++ src/workshop/oop/flexible/Employee.java
@@ -1,7 +1,9 @@
 package workshop.oop.flexible;
 
 /*
- * 추상클래스는 객체를 생성할 수 없다. Employee e = new Employee(); (x)
+ * 추상클래스는 객체를 생성할 수 없다. 
+ * Employee e = new Employee(); (x)
+ * Employee m = new Manger(); (O)
  * 추상메서드를 하나 이상 가지고 있으면 무조건 추상클래스가 된다.
  * Super 클래스의 메서드가  추상메서드이면 , Sub 클래스에서 그 추상메서드를 
  * 반드시 오버라이딩 해야 한다. 
--- src/workshop/oop/flexible/MereClerk.java
+++ src/workshop/oop/flexible/MereClerk.java
@@ -7,6 +7,11 @@ public class MereClerk extends Employee {
     	super(name,salary);
     }
     
+    /*
+     * 오버라이딩 규칙은 부모가 상속해준 메서드와 Signature가 반드시 같아야 한다.
+     * @Override 어노테이션은 오버라이딩 규칙을 컴파일타임에 체크해준다.
+     */
+    @Override
     public void manageSalary(double rate) {
         salary = salary+ salary*(rate/100);
     }
