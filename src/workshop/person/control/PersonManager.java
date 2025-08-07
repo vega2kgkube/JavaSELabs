db706e6d5f1910274f4ecb074f4bd44ef29f6eb3
vega2kgkube <vega2kgkube@gmail.com>
2025-08-07 09:46:38 +0900

    PersonManager fillPersons

--- src/workshop/person/control/PersonManager.java
+++ src/workshop/person/control/PersonManager.java
@@ -12,6 +12,15 @@ public class PersonManager {
 		//배열선언 및 초기화
 		PersonEntity[] persons = new PersonEntity[10];
 		//persons 변수는 PersonEntity[] 타입이고, persons[0]은 PersonEntity 타입이다.
+		personMgr.fillPersons(persons);
+		
+		//for loop를 순회하면서 
+		for(PersonEntity person:persons) {
+			System.out.println(person.getName() + " " + person.getGender());
+		}
+	}
+
+	public void fillPersons(PersonEntity[] persons) {
 		persons[0] = new PersonEntity("이성호","7212121028102", "인천 계양구", "032-392-2932");
 		persons[1] = new PersonEntity("김하늘","7302132363217", "서울 강동구", "02-362-1932");
 		persons[2] = new PersonEntity("박영수","7503111233201", "서울 성북구", "02-887-1542");
@@ -22,11 +31,6 @@ public class PersonManager {
 		persons[7] = new PersonEntity("유성미","7103282025101", "서울 은평구", "02-452-0939");
 		persons[8] = new PersonEntity("황재현","7806231031101", "인천 중구", "032-327-2202");
 		persons[9] = new PersonEntity("최철수","7601211025101", "인천 계양구", "032-122-7832");
-		
-		//for loop를 순회하면서 
-		for(PersonEntity person:persons) {
-			System.out.println(person.getName() + " " + person.getGender());
-		}
 	}
 	
 	public void printTitle(String title) {
