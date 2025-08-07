5852eda5c42554cbc016103382738a3fdf9a7dbc
vega2kgkube <vega2kgkube@gmail.com>
2025-08-07 13:40:23 +0900

    ArrayList사용하기

--- src/workshop/person/control/PersonManagerCollection.java
+++ src/workshop/person/control/PersonManagerCollection.java
@@ -26,7 +26,7 @@ public class PersonManagerCollection {
 		List<PersonEntity> personList = new ArrayList<>();
 		
 		//persons 변수는 PersonEntity[] 타입이고, persons[0]은 PersonEntity 타입이다.
-		personMgr.fillPersons(persons);		
+		personMgr.fillPersons(personList);		
 		
 		personMgr.printTitle("인물정보 조회시스템");
 		personMgr.showPerson(persons);		
@@ -69,17 +69,17 @@ public class PersonManagerCollection {
 		}
 	}
 
-	public void fillPersons(PersonEntity[] persons) {
-		persons[0] = new PersonEntity("이성호","7212121028102", "인천 계양구", "032-392-2932");
-		persons[1] = new PersonEntity("김하늘","7302132363217", "서울 강동구", "02-362-1932");
-		persons[2] = new PersonEntity("박영수","7503111233201", "서울 성북구", "02-887-1542");
-		persons[3] = new PersonEntity("나인수","7312041038988", "대전 유성구", "032-384-2223");
-		persons[4] = new PersonEntity("홍정수","7606221021341", "서울 양천구", "02-158-7333");
-		persons[5] = new PersonEntity("이미숙","7502142021321", "서울 강서구", "02-323-1934");
-		persons[6] = new PersonEntity("박성구","7402061023101", "서울 종로구", "02-308-0932");
-		persons[7] = new PersonEntity("유성미","7103282025101", "서울 은평구", "02-452-0939");
-		persons[8] = new PersonEntity("황재현","7806231031101", "인천 중구", "032-327-2202");
-		persons[9] = new PersonEntity("최철수","7601211025101", "인천 계양구", "032-122-7832");
+	public void fillPersons(List<PersonEntity> persons) {
+		persons.add(new PersonEntity("이성호","7212121028102", "인천 계양구", "032-392-2932"));
+		persons.add(new PersonEntity("김하늘","7302132363217", "서울 강동구", "02-362-1932"));
+		persons.add(new PersonEntity("박영수","7503111233201", "서울 성북구", "02-887-1542"));
+		persons.add(new PersonEntity("나인수","7312041038988", "대전 유성구", "032-384-2223"));
+		persons.add(new PersonEntity("홍정수","7606221021341", "서울 양천구", "02-158-7333"));
+		persons.add(new PersonEntity("이미숙","7502142021321", "서울 강서구", "02-323-1934"));
+		persons.add(new PersonEntity("박성구","7402061023101", "서울 종로구", "02-308-0932"));
+		persons.add(new PersonEntity("유성미","7103282025101", "서울 은평구", "02-452-0939"));
+		persons.add(new PersonEntity("황재현","7806231031101", "인천 중구", "032-327-2202"));
+		persons.add(new PersonEntity("최철수","7601211025101", "인천 계양구", "032-122-7832"));
 	}
 	
 	public void printTitle(String title) {
