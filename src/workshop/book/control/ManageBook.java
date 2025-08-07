da5a15c7dc5f66df34a2853b8d9583baaefddd92
vega2kgkube <vega2kgkube@gmail.com>
2025-08-07 17:08:08 +0900

    ManageBook toString()

--- src/workshop/book/control/ManageBook.java
+++ src/workshop/book/control/ManageBook.java
@@ -1,8 +1,14 @@
 package workshop.book.control;
 
+import workshop.book.entity.Magazine;
+import workshop.book.entity.Novel;
+import workshop.book.entity.Publication;
+import workshop.book.entity.ReferenceBook;
+
 public class ManageBook {
 
 	public static void main(String[] args) {
+		//Publication 타입의 배열을 선언 및 생성
 		Publication[] pubs = new Publication[5];
 		pubs[0] = new Magazine("마이크로소프트","2007-10-01",328,9900,"매월");
 		pubs[1] = new Magazine("경영과컴퓨터","2007-10-03",316,9000,"매월");
@@ -10,6 +16,9 @@ public class ManageBook {
 		pubs[3] = new Novel("남한산성","2007-04-14",383,11000,"김훈","대하소설");
 		pubs[4] = new ReferenceBook("실용주의프로그래머","2007-01-14",496,25000,"소프트웨어공학");	
 
+		for(Publication pub : pubs) {
+			System.out.println(pub.toString());
+		}
 	}
 
 }
