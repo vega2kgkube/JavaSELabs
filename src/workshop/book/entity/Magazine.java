d60b4dd18e1ad0e8fe59afe36b541bdb49d1bdbe
vega2kgkube <vega2kgkube@gmail.com>
2025-08-07 16:59:33 +0900

    Magazine add

---
+++ src/workshop/book/entity/Magazine.java
@@ -0,0 +1,15 @@
+package workshop.book.entity;
+
+public class Magazine extends Publication {
+	private String publishingPeriod;
+
+	public Magazine() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public Magazine(String title, String publishingDate, int page, int price) {
+		super(title, publishingDate, page, price);
+		// TODO Auto-generated constructor stub
+	}
+
+}
