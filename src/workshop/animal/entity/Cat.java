89b197d11006bda1f52d7d6d6c412937c99484a8
vega2kgkube <vega2kgkube@gmail.com>
2025-08-08 09:39:13 +0900

    Cat add

---
+++ src/workshop/animal/entity/Cat.java
@@ -0,0 +1,31 @@
+package workshop.animal.entity;
+
+public class Cat extends Animal implements Pet {
+	private String name;
+
+	@Override
+	public String getName() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void setName(String name) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void play() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void eat() {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	
+}
