35d99e459be50c704fb3dc39d23696534749a0f7
vega2kgkube <vega2kgkube@gmail.com>
2025-08-08 09:25:43 +0900

    Animal update

--- src/workshop/animal/entity/Animal.java
+++ src/workshop/animal/entity/Animal.java
@@ -1,6 +1,15 @@
 package workshop.animal.entity;
 
-public class Animal {
+public abstract class Animal {
 	protected int legs;
 	
+	protected Animal(int legs) {
+		this.legs = legs;
+	}
+	
+	public abstract void eat();
+	
+	public void walk() {
+		System.out.println("동물은 " + this.legs + "개의 다리로 걷는다!");
+	}
 }
